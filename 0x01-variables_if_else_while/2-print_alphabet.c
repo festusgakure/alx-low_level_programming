e0ea2119bd8bd53cc3a9c3372f5038bf72dad030
festusgakure <festusgakure@gmail.com>
2023-03-17 12:30:02 -0700

    Added file 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Prints alphabets in lower case
+ * Return: Always 0
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
