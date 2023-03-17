4be603b6919b3aa75113ff7b43939dce0971c2b3
festusgakure <festusgakure@gmail.com>
2023-03-17 15:28:11 -0700

    Added file 7

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,7 +3,6 @@
  * main - Entry point
  * return: Always 0.
  */
-
 int main(void)
 {
 	int i;
---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Prints alphabets in lower case
+ * Return: Always 0
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z'; ch >= 'a'; ch--)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
