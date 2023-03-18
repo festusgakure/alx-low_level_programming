c3156a2289e12601c4ea04a699f055ac72c93636
festusgakure <festusgakure@gmail.com>
2023-03-18 02:18:31 -0700

    Not giving up

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,7 +3,7 @@
 /**
  * main - Entry point
  * Description: prints all single digit numbers of base 10 starting from 0
- * return: Always 0 (success)
+ * Return: Always 0 (success)
  */
 int main(void)
 {
