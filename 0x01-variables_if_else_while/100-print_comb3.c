a577e878089a30ef901a5a1eb02765f6a37f239a
festusgakure <festusgakure@gmail.com>
2023-03-19 14:57:11 -0700

    Corrected file 100

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,26 +6,21 @@
  */
 int main(void)
 {
-	int i, j, k;
+	int i, j;
 
 	for (i = '0'; i <= '9'; i++)
 	{
 		for (j = '0'; j <= '9'; j++)
 		{
-			for(k = '0'; k <= '9'; k++)
+			if (i < j)
 			{
-
-				if (i < j) && (j<k)
-				{
-					putchar(i);
-					putchar(j);
-					putchar(k);
-				if (i != '7' && j != '8' && k != '9')
+				putchar(i);
+				putchar(j);
+				if (i != '7' && j != '8')
 				{
 					putchar(',');
 					putchar(' ');
 				}
-			}
 			}
 		}
 	}
