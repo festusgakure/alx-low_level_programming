0a260d60419be0b000bf92afee1ac54cc2430b65
festusgakure <festusgakure@gmail.com>
2023-03-20 02:35:50 -0700

    Corrected file 4

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main- Entry point
- * return-always 1
+ * main - Entry point
+ * return: Always 1 (success)
  */
-int main(void)
+int main(void)/*This is the stating point*/
 {
 	write(1,
 	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
