12f8c23a2795da2cf2af2d4e243a956ed1822afe
festusgakure <festusgakure@gmail.com>
2023-03-21 14:40:31 -0700

    Added file 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * print_sign - prints the sign of a number
+ * @n: Character to compare
+ * Return: 1, 0 and -1
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		_putchar(',');
+		_putchar(' ');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		_putchar(',');
+		_putchar(' ');
+		return (-1);
+	}
+	return (0);
+}
