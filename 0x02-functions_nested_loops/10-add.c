9360beaf738f157a836b23c050ba8e41d2780801
festusgakure <festusgakure@gmail.com>
2023-03-22 10:04:48 -0700

    Corrected file 10

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * int add - Adds two integers and returns the result
+ * add - Adds two integers and returns the result
  * @a: Hold the value of 1st int
  * @b: Holds the value of 2nd int
  * Return: Sum of the integers
