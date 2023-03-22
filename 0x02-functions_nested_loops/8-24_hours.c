5c1a21d4bf0cd1b9fdb6a7aa1662c427cb525578
festusgakure <festusgakure@gmail.com>
2023-03-21 23:56:16 -0700

    Added file 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,5 +7,24 @@
  */
 void jack_bauer(void)
 {
-	return (0);
+	int a, b, c, d;
+
+	for (a = '0'; a <= '2'; a++)
+	{
+		for (b = '0'; b <= '3'; b++)
+		{
+			for (c = '0'; c <= '5'; c++)
+			{
+				for (d = '0'; d <= '9'; d++)
+				{
+					_putchar(a);
+					_putchar(b);
+					_putchar(':');
+					_putchar(c);
+					_putchar(d);
+					_putchar('\n');
+				}
+			}
+		}
+	}
 }
