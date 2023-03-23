127836b708ed47ea06158fade6d8cebc082a5bf6
festusgakure <festusgakure@gmail.com>
2023-03-23 14:26:52 -0700

    Made correction on file 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -10,9 +10,16 @@ void print_line(int n)
 {
 	int i;
 
-	for (i = 0; i <= n; i++)
+	if (n != 0 && n > 0)
 	{
-		_putchar(95);
+		for (i = 0; i <= n; i++)
+		{
+			_putchar(95);
+		}
+		_putchar('\n');
 	}
-	_putchar('\n');
+	else
+	{
+		_putchar('\n');
+	}
 }
