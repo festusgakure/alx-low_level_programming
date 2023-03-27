307e33ce6c4a7243460b4a3eab0e9a5bd97f3ee7
festusgakure <festusgakure@gmail.com>
2023-03-27 10:42:05 -0700

    Fixed Betty style and documentation

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,13 +2,14 @@
 
 /**
  * _strlen - returns the length of a string
- * s: The string whose length is determined
+ * @s: The string whose length is determined
  * Return: The length of the string
  */
 
 int _strlen(char *s)
 {
 	int c;
+
 	c = 0;
 
 	while (s[c] != '\0')
