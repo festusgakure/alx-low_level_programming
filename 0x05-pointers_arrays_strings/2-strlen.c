fc89d0c4572a0d65d1e01fc332964ad768b7aaaa
festusgakure <festusgakure@gmail.com>
2023-03-27 10:38:57 -0700

    Added file 2 that counts length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * s: The string whose length is determined
+ * Return: The length of the string
+ */
+
+int _strlen(char *s)
+{
+	int c;
+	c = 0;
+
+	while (s[c] != '\0')
+		c++;
+	return (c);
+}
