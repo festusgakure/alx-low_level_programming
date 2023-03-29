0944516f7af358811fbb73690e964ccc7e9d0773
festusgakure <festusgakure@gmail.com>
2023-03-29 06:43:52 -0700

    Added file 1 that concatenates two strings

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates two strings
+ * @dest: First string
+ * @src: Second string
+ * @n: Number of bytes in src
+ * Return: Concatenated string
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	int j;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	for (j = 0; j < n; j++)
+	{
+		dest[i] = src[j];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
