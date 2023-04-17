4717f4f5aa053937f3739cc56b54297f72be2f38
festusgakure <festusgakure@gmail.com>
2023-04-17 09:36:05 -0700

    Added file 4 that creates a new dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,45 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * new_dog - creates a new dog
+ * @name: Element 1
+ * @age: Element 2
+ * @owner: Element 3
+ * Return: new dog
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *p;
+	char *newname, *newowner;
+	int len1, len2, i;
+
+	p = malloc(sizeof(dog_t));
+	if (p == NULL)
+		return (NULL);
+	for (len1 = 0; name[len1]; len1++)
+	;
+	for (len2 = 0; owner[len2]; len2++)
+	;
+	len1++;
+	len2++;
+	newname = malloc(len1);
+	if (!newname)
+	{
+		return (NULL);
+	}
+	newowner = malloc(len2);
+	if (!newowner)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < len1; i++)
+		newname[i] = name[i];
+	for (i = 0; i < len2; i++)
+		newowner[i] = owner[i];
+	p->name = newname;
+	p->owner = newowner;
+	p->age = age;
+	return (p);
+}
