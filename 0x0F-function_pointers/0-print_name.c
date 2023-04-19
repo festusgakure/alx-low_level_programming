d023c8964a10a8596114af69d61b341c309e89d6
festusgakure <festusgakure@gmail.com>
2023-04-19 08:58:36 -0700

    FIxed file 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,11 +4,12 @@
  * print_name -  prints a name
  * @name: A sting to be printed
  * @f: pointer to function
+ * Return: void
  */
 
 void print_name(char *name, void (*f)(char *))
 {
-	if (name == NULL)
+	if (name == NULL || f == NULL)
 		return;
 	f(name);
 }
