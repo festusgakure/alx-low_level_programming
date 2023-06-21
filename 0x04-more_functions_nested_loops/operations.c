38c6145ef9f7a0f4c7bdba675bc31b19209a3d2e
festusgakure <festusgakure@gmail.com>
2023-06-21 01:39:28 -0700

    Creates a dynamic library that contains C functions that can be called from Python

---
+++ 0x04-more_functions_nested_loops/operations.c
@@ -0,0 +1,27 @@
+// operations.c
+
+// Function 1: Add two integers
+int add(int a, int b) {
+    return a + b;
+}
+
+// Function 2: Subtract two integers
+int sub(int a, int b) {
+    return a - b;
+}
+
+// Function 3: Multiply two integers
+int mul(int a, int b) {
+    return a * b;
+}
+
+// Function 4: Divide two integers (integer division)
+int div(int a, int b) {
+    return a / b;
+}
+
+// Function 5: Calculate the remainder of division
+int mod(int a, int b) {
+    return a % b;
+}
+
